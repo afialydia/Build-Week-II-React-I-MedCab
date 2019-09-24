e9f4c4c51f047bdf3d754dc73fb7d218831b5d9b
Jackie Atwood <takalaj@verizon.net>
2019-09-24 11:49:26 -0400

    App Changes

--- med-cabinet/src/App.js
+++ med-cabinet/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from "./Components/Header";
 import './App.css';
 import ProfileCard from "./Components/ProfileCard";
+// import { ProfileTitle } from "./Components/Styles";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      {/* <ProfileTitle>Our Top Recommendations</ProfileTitle> */}
       <ProfileCard />
     </div>
   );
