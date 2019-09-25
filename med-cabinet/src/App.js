4722f80d7f4d8e1483394d1107c8e4d4b2464463
Afia-Lydia <you@example.com>
2019-09-25 17:50:05 -0400

    test

--- med-cabinet/src/App.js
+++ med-cabinet/src/App.js
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
-import Header from "./Components/Header";
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
 import Profile from "./Components/Profile";
 import Entrance from "./Components/Entrance";
 import SearchForm from "./Components/SearchForm";
-import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import {ProtectedRoute} from './Components/ProtectedRoute'
 
 
@@ -18,12 +17,11 @@ function App() {
     <div className="App">
       <Switch>
       <Route exact path='/' component={Entrance}/>
-      <Route exact path='/search' component={SearchForm}/>
+      <Route exact path='/search' render={(props) => <SearchForm favorites={favorites} setFavorites={setFavorites}/>}/>
       <ProtectedRoute exact path='/profile' component={Profile}/>
       <Route path ="*" component={()=> "404 not found"}/>
       </Switch>
 
-      <Route exact path='/search' render={(props) => <SearchForm favorites={favorites} setFavorites={setFavorites}/>}/>
       <Route exact path='/profile' render={(props) => <Profile favorites={favorites}/>}/>
 
     </div>
