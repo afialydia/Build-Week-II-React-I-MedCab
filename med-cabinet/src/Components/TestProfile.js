d22525477c700e4e55f347d870b2f35f275cbea8
Dom <domeccleston@gmail.com>
2019-09-26 21:57:27 +0100

    fixed profile routing

--- med-cabinet/src/Components/TestProfile.js
+++ med-cabinet/src/Components/TestProfile.js
@@ -1,15 +1,27 @@
 import React from "react";
 import { Wrapped } from "./Styles";
 import RecCards from './RecCards';
-import ProfileTitle from "./ProfileTitle";
+import Test from "./ProfileTitle"
+import Header from "./Header";
+import { ProfilePage } from "./Styles";
 
 const TestProfile = ({ favorites, setFavorites }) => {
 
+  console.log(favorites);
+
   return (
+    <div>
+    <ProfilePage className="Profile">
+    <Header />
     <Wrapped>
-        <ProfileTitle />
-        {favorites.map(favorite => <RecCards data={favorite} favorites={favorites} setFavorites={setFavorites}></RecCards>)}
+        <Test />
+         {favorites.map(favorite => <RecCards data={favorite} favorites={favorites} setFavorites={setFavorites}></RecCards>)}
     </Wrapped>
+    </ProfilePage>
+
+    </div>
   );}
 
 export default TestProfile;
+
+
