03a966cb08f99fdc9666444efa4aade5b1842005
Dom <domeccleston@gmail.com>
2019-09-26 11:07:19 +0100

    /profile route is now protected so back to working on /testprofile. Fixed styling issue with Effects and Flavors field (needed space after comma

--- med-cabinet/src/Components/RecCards.js
+++ med-cabinet/src/Components/RecCards.js
@@ -6,14 +6,14 @@ function RecCards({ data, favorites, setFavorites }) {
     const handleDelete = () => {
         setFavorites(favorites.filter(item => item.Strain !== data.Strain))
     }
-
+    console.log(data.Effects.split(',').join(', '))
     return (
         <div>
             <One className= "recommendation-one">
                 <Info>Strain: {data.Strain}</Info>
                 <Info>Type: {data.Type}</Info>                                              
-                <Info>Flavors: {data.Flavor}</Info>                  
-                <Info>Effects: {data.Effects}</Info>      
+                <Info>Flavors: {data.Flavor.split(',').join(', ')}</Info>                  
+                <Info>Effects: {data.Effects.split(',').join(', ')}</Info>      
                 <Button onClick={()=> handleDelete()}>Delete Selection</Button>
             </One>
         </div>
--- med-cabinet/src/Components/TestProfile.js
+++ med-cabinet/src/Components/TestProfile.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React from "react";
+import { Wrapped } from "./Styles";
 import RecCards from './RecCards';
+import ProfileTitle from "./ProfileTitle";
 
-export default function TestProfile({ favorites }) {
-    console.log(favorites);
-    return (
-        favorites.map(favorite => <RecCards data={favorite} favorites={favorites}></RecCards>)
-    )
-}
+const TestProfile = ({ favorites, setFavorites }) => {
+
+  return (
+    <Wrapped>
+        <ProfileTitle />
+        {favorites.map(favorite => <RecCards data={favorite} favorites={favorites} setFavorites={setFavorites}></RecCards>)}
+    </Wrapped>
+  );}
+
+export default TestProfile;
